4bfab105204ac10273dde8fc8ef361e106c7924e
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2020-07-02 15:18:34 -0300

    Pending changes for chainaddresses

--- src/main/java/org/rif/notifier/managers/datamanagers/DataFetcherManager.java
+++ src/main/java/org/rif/notifier/managers/datamanagers/DataFetcherManager.java
@@ -2,8 +2,6 @@ package org.rif.notifier.managers.datamanagers;
 
 import org.rif.notifier.models.entities.DataFetcherEntity;
 import org.rif.notifier.repositories.DataFetcherRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,10 +24,29 @@ public class DataFetcherManager {
         return dataFetcherRepository.save(dt);
     }
 
+    public DataFetcherEntity insertLastBlockChainAddress(BigInteger lastBlock){
+        DataFetcherEntity dt;
+        List<DataFetcherEntity> lst = dataFetcherRepository.findAll();
+        if(lst.size() > 1)
+            dt = lst.get(1);
+        else
+            dt = new DataFetcherEntity();
+        dt.setLastBlock(lastBlock);
+        return dataFetcherRepository.save(dt);
+    }
+
     public BigInteger get(){
         List<DataFetcherEntity> lst = dataFetcherRepository.findAll();
         if(lst.size() > 0)
             return lst.get(0).getLastBlock();
+        else
+            return new BigInteger("0");
+    }
+
+    public BigInteger getBlockChainAddresses(){
+        List<DataFetcherEntity> lst = dataFetcherRepository.findAll();
+        if(lst.size() > 1)
+            return lst.get(1).getLastBlock();
         else
             return new BigInteger("0");
     }
--- src/main/java/org/rif/notifier/scheduled/DataFetchingJob.java
+++ src/main/java/org/rif/notifier/scheduled/DataFetchingJob.java
@@ -18,8 +18,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.web3j.abi.TypeReference;
+import org.web3j.utils.Numeric;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.abi.datatypes.generated.Bytes32;
+import org.web3j.abi.datatypes.generated.Bytes4;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -31,6 +34,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
+import static org.rif.notifier.constants.CommonConstants.RSK_SLIP_ADDRESS;
 import static org.rif.notifier.constants.TopicParamTypes.*;
 import static org.rif.notifier.constants.TopicTypes.*;
 
@@ -52,6 +56,9 @@ public class DataFetchingJob {
     @Value("${rsk.blockchain.tokennetworkregistry}")
     private String tokenNetworkRegistry;
 
+    @Value("${rsk.blockchain.multichaincontract}")
+    private String multiChainContract;
+
     private boolean fetchedTokens = false;
 
     @Value("${notifier.blocks.startFromLast}")
@@ -66,12 +73,13 @@ public class DataFetchingJob {
         // Get latest block for this run
         BigInteger to = rskBlockchainService.getLastBlock();
         BigInteger from;
-        if(onInit) {
+        BigInteger fromChainAddresses;
+        if (onInit) {
             //Saving lastblock so it starts fetching from here
             dbManagerFacade.saveLastBlock(to);
             from = to;
             onInit = false;
-        }else {
+        } else {
             from = dbManagerFacade.getLastBlock();
             from = from.add(new BigInteger("1"));
         }
@@ -173,6 +181,51 @@ public class DataFetchingJob {
         }else{
             logger.info(Thread.currentThread().getId() + " - Nothing to fetch yet -");
         }
+    }
+
+    @Scheduled(fixedRateString = "${notifier.run.fixedRateFetchingChainAddresses}", initialDelayString = "${notifier.run.fixedDelayFetchingChainAddresses}")
+    public void runChainAddresses() throws Exception {
+        // Get latest block for this run
+        BigInteger to = rskBlockchainService.getLastBlock();
+        BigInteger fromChainAddresses;
+        fromChainAddresses = dbManagerFacade.getLastBlockForChainAddresses();
+        fromChainAddresses = fromChainAddresses.add(new BigInteger("1"));
+        long start = System.currentTimeMillis();
+        List<CompletableFuture<List<FetchedEvent>>> chainAddresses = new ArrayList<>();
+        chainAddresses.add(rskBlockchainService.getContractEvents(getAddrChanged(), fromChainAddresses, to));
+        chainAddresses.add(rskBlockchainService.getContractEvents(getChainAddrChanged(), fromChainAddresses, to));
+
+        chainAddresses.forEach(listCompletableFuture -> {
+            listCompletableFuture.whenComplete((fetchedEvents, throwable) -> {
+                List<ChainAddressEvent> chainsEvents = new ArrayList<>();
+                long end = System.currentTimeMillis();
+                logger.info(Thread.currentThread().getId() + " - End fetching chainaddres = " + (end - start));
+                logger.info(Thread.currentThread().getId() + " - Completed fetching chainaddresses: " + fetchedEvents.size());
+                // I need to filter by topics, cause here we have chainaddresses events for RSK and other chains that has different params in the event
+                fetchedEvents.stream().filter(item -> item.getTopicId() == -2).forEach(item -> {
+                    // RSK AddrChanged event
+                    String nodehash = Numeric.toHexString((byte[]) item.getValues().get(0).getValue());
+                    String eventName = "AddrChanged";
+                    String address = item.getValues().get(1).getValue().toString();
+                    ChainAddressEvent rskChain = new ChainAddressEvent(nodehash, eventName, RSK_SLIP_ADDRESS, address);
+                    chainsEvents.add(rskChain);
+                });
+                fetchedEvents.stream().filter(item -> item.getTopicId() == -3).forEach(item -> {
+                    // ChainAddrChanged event
+                    String nodehash = Numeric.toHexString((byte[]) item.getValues().get(0).getValue());
+                    String chain = Numeric.toHexString((byte[]) item.getValues().get(1).getValue());
+                    String address = item.getValues().get(2).getValue().toString();
+                    String eventName = "ChainAddrChanged";
+                    ChainAddressEvent chainAddr = new ChainAddressEvent(nodehash, eventName, chain, address);
+                    chainsEvents.add(chainAddr);
+                });
+
+                if (!chainsEvents.isEmpty()) {
+                    dbManagerFacade.saveChainAddressesEvents(chainsEvents);
+                }
+            });
+        });
+        dbManagerFacade.saveLastBlockChainAdddresses(to);
     }
 
     /**
@@ -296,9 +349,32 @@ public class DataFetchingJob {
      * @return EthereumBasedListenable to get tokens
      */
     private EthereumBasedListenable getTokensNetwork(){
-        return new EthereumBasedListenable(tokenNetworkRegistry, EthereumBasedListenableTypes.CONTRACT_EVENT, Arrays.asList(
+        return new EthereumBasedListenable(multiChainContract, EthereumBasedListenableTypes.CONTRACT_EVENT, Arrays.asList(
                 new TypeReference<Address>(true) {},
                 new TypeReference<Address>(true) {}
         ), "TokenNetworkCreated", -1);
+    }
+
+    /**
+     * Creates a EthereumBasedListeneable to get all events AddrChanged
+     * @return EthereumBasedListenable to get event AddrChanged
+     */
+    private EthereumBasedListenable getAddrChanged(){
+        return new EthereumBasedListenable(multiChainContract, EthereumBasedListenableTypes.CONTRACT_EVENT, Arrays.asList(
+                new TypeReference<Bytes32>(true) {},
+                new TypeReference<Address>(false) {}
+        ), "AddrChanged", -2);
+    }
+
+    /**
+     * Creates a EthereumBasedListeneable to get all events chainAddrChanged
+     * @return EthereumBasedListenable to get event chainAddrChanged
+     */
+    private EthereumBasedListenable getChainAddrChanged(){
+        return new EthereumBasedListenable(multiChainContract, EthereumBasedListenableTypes.CONTRACT_EVENT, Arrays.asList(
+                new TypeReference<Bytes32>(true) {},
+                new TypeReference<Bytes4>(false) {},
+                new TypeReference<Utf8String>(false) {}
+        ), "ChainAddrChanged", -3);
     }
 }
